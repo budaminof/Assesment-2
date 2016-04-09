68f1683e693144980e1956cc87fba54278b5ee5a
bud aminof <budaminof@gmail.com>
2016-04-08 22:25:34 -0600

    heroku

--- knexfile.js
+++ knexfile.js
@@ -6,12 +6,21 @@ module.exports = {
        client: 'postgresql',
        connection: process.env.DATABASE_URL
        },
+       pool: {
+        min:2,
+        max:10,
+       },
        seeds: {
             directory: './seeds/'
-        },
+        }
+
     prodution: {
       client: 'postgresql',
       connection: process.env.DATABASE_UR
-      }
-      
+        },
+      pool: {
+       min:2,
+       max:10,
+      },
+
 };
